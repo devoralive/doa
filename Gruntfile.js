feb4a954978d78365f6b75489f1ad9b4c0d1ba6f
Nicolas Dufreche <n.dufreche@gmail.com>
2014-11-24 23:15:50 +0100

    Removing useless package

--- Gruntfile.js
+++ Gruntfile.js
@@ -110,7 +110,6 @@ module.exports = function (grunt) {
 
     grunt.loadNpmTasks('grunt-jslint');
     grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-jasmine');
     grunt.loadNpmTasks('grunt-istanbul-coverage');
 
